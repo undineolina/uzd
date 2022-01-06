461240561a9cae386c5e794fc876dc2a6403bfa6
Undine Olina <97155884+undineolina@users.noreply.github.com>
2022-01-06 07:27:49 +0000

    izpildiju 5.uzdevumu

---
+++ main.py
@@ -0,0 +1,30 @@
+import math
+a=float(input("Ievadi skaitli:"))
+def decimal(a):
+  s=int(a)
+  r=a-s
+  return r
+
+def kvadrats(a):
+  x=math.pow(a,2)
+  return x
+
+def sakne(a):
+  if a>=0:
+    z=math.sqrt(a)
+  else:
+    z="nav"
+  return z
+
+def pozitivs(a):
+  if a>0:
+    x="+"
+  elif a==0:
+    x=0
+  else:
+    x="-"
+  return x
+print(decimal(a))
+print(kvadrats(a))
+print(sakne(a))
+print(pozitivs(a))
